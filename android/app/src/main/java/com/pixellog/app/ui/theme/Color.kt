55326c8b9f59437f65dabbb97dbd30871a4c97e2
PixelLog <pixellog@localhost>
2026-08-29 03:14:50 +0300

    Clear repository

--- android/app/src/main/java/com/pixellog/app/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.pixellog.app.ui.theme
-import androidx.compose.ui.graphics.Color
-
-val Neutral950 = Color(0xFF0a0a0a)
-val Neutral900 = Color(0xFF171717)
-val Neutral800 = Color(0xFF262626)
-val Indigo500 = Color(0xFF6366f1)
-val Indigo600 = Color(0xFF4f46e5)
-val TextGray = Color(0xFFA3A3A3)
